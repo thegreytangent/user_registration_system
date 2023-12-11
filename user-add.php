f49de8135e4334ca4ea61b75337bd28b99a86c58
thegreytangent <thegreytangent@gmail.com>
2023-12-11 09:31:27 +0800

    Discussing about alerts

--- user-add.php
+++ user-add.php
@@ -2,15 +2,27 @@
 
 <?php 
 
-
+//Get all information and store it inside the variable
 $username = Input::get('username');
 $password = Input::get('password');
 $confirm_password = Input::get('confirm_password');
 $firstname = Input::get('firstname');
 $lastname = Input::get('lastname');
 
+//create user
+// $user = new User();
+// $user->username = $username;
+// $user->password  = $user->securePassword();
+// $user->create();
 
-$user = new User();
-$user->username = $username;
-$user->password  = $user->securePassword();
-$user->create();
+//Redirect back to the form
+$isExists = false;
+
+if ($isExists) {
+
+    header("location: index.php?success=false"); // query parameters
+} else {
+    header("location: index.php?success=true"); // query parameters
+}
+
+
